002f89d29515dc6f4108e9f1ae86a1da6ae89888
kiburei <akiburei@gmail.com>
2017-07-11 11:21:27 +0300

    create DB, rund bundler for sinatra and pg

---
+++ app.rb
@@ -0,0 +1,9 @@
+require "sinatra"
+require "sinatra/reloader"
+require "pg"
+require "./lib/animal"
+require "./lib//breed"
+require "./lib/customer"
+also_reload("lib/**/*.rb")
+
+DB=PG.connect({:dbname=>"animal_shelter"})
